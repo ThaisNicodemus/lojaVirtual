dd76598ada6bb83d920f317e938e743c8ac96cad
Thais Nicodemus <thaisnicodemus@gmail.com>
2020-10-02 20:13:16 -0300

    criando o projeto

---
+++ lojaVirtual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lojaVirtual
+//
+//  Created by Thais Nicodemus on 02/10/20.
+//  Copyright © 2020 Thais Nicodemus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
